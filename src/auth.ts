a83d573561b1bd7676b845672bf1fc9655dbd9a2
Gavin Gregory <gavgrego@gmail.com>
2025-04-02 21:48:53 -0700

    add email verification to authentication flow

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,22 @@
 import { betterAuth } from 'better-auth';
 import { Pool } from 'pg';
 import { passkey } from 'better-auth/plugins/passkey';
+import sendEmail from './actions/sendEmail';
 
 export const auth = betterAuth({
   database: new Pool({
     connectionString: process.env.BETTER_AUTH_STRING,
   }),
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      await sendEmail({
+        to: user.email,
+        subject: 'Verify your email address for points.credit',
+        text: `Click the link to verify your email: ${url}`,
+      });
+    },
+  },
   user: {
     additionalFields: {
       cards: {
@@ -21,6 +32,7 @@ export const auth = betterAuth({
   },
   emailAndPassword: {
     enabled: true,
+    requireEmailVerification: true,
   },
   plugins: [passkey()],
 });
